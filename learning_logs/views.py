df509801bb61ca5e96b59cfa250d81fc0477d7f5
Arthur_linux <iamleary@live.com>
2024-02-05 18:04:45 -0500

    c19, add new_topic function

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Topic
+from .forms import TopicForm
 
 # Create your views here.
 def index(request):
@@ -19,3 +22,19 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added') #show latest first
     context = {'topic':topic, 'entries':entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """add usr's new topic"""
+    if request.method != 'POST': #this is GET
+        form = TopicForm() # if not submitting a form, new a form
+        print('new topic Get')
+    else:# this is POST
+        print('new topic POST')
+        #POST submitted data
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+        
+    context = {'form':form}
+    return render(request, 'learning_logs/new_topic.html', context)
